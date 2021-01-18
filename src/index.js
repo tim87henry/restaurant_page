151f2f3d53ff69a9f805dfd0bba6af3706669df9
Timothy <tim87henry@gmail.com>
2021-01-18 22:19:35 +1100

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function component() {
+    const element = document.createElement('div');
+  
+    element.innerHTML = "Pazzo's cafe"
+  
+    return element;
+  }
+  
+//document.body.appendChild(component());
