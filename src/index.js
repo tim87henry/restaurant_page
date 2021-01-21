87fc4cb7b764e13d0a093591dd03093508c4e305
Timothy <tim87henry@gmail.com>
2021-01-21 23:02:53 +1100

    Initiated tabbed layout code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import init_load from './init';
 
+const banner = document.createElement('img');
+banner.src= "../banner.png";
+document.body.appendChild(banner);
+
+const about_title = document.createElement('button');
+about_title.innerHTML="About Us";
+about_title.className="h1_text";
+about_title.onclick=opentab;
+document.body.appendChild(about_title);
+
+const menu_title = document.createElement('button');
+menu_title.innerHTML="Menu";
+menu_title.className="h1_text";
+menu_title.onclick=opentab;
+document.body.appendChild(menu_title);
+
+const contact_title = document.createElement('button');
+contact_title.innerHTML="Contact Us";
+contact_title.className="h1_text";
+contact_title.onclick=opentab;
+document.body.appendChild(contact_title);
+
 init_load();
+
+function opentab () {
+    //alert("Display individual tabs here");
+    var pages;
+
+    pages=document.getElementsByClassName("page_content");
+    for (var i=0;i<pages.length;i++) {
+        pages[i].style.display="none";
+    }
+}
--- src/init.js
+++ src/init.js
@@ -1,23 +1,15 @@
 function init_load() {
-    const banner = document.createElement('img');
-    banner.src= "../banner.png";
-    document.body.appendChild(banner);
-
+    
     const about = document.createElement('div');
-    about.innerHTML="<br>"+"About Us"+"<br>";
-    about.className="h1_text";
-    document.body.appendChild(about);
-
-    const descr = document.createElement('div');
-    descr.innerHTML="<br>"+"Pazzo's Cafe is an authentic Italian cafe, established in the 5th century BC."+"<br>"+
+    about.className="page_content";
+    const about_text = document.createElement('div');
+    about_text.innerHTML="<br>"+"Pazzo's Cafe is an authentic Italian cafe, established in the 5th century BC."+"<br>"+
     "We even sold take-away Lattes to Cleopatra and Julius Ceaser. In fact, we invented coffee.";
-    document.body.appendChild(descr);
+    about.appendChild(about_text);
+    document.body.append(about);
 
-    const menu_title = document.createElement('div');
-    menu_title.innerHTML="<br>"+"Menu"+"<br>";
-    menu_title.className="h1_text";
-    document.body.appendChild(menu_title);
-
+    const menu = document.createElement('div');
+    menu.className="page_content";
     const menu_items = document.createElement('div');
     menu_items.innerHTML="<br>"+"Latte  -  $5.00"+"<br>"+
     "Capuccino  -  $5.50"+"<br>"+
@@ -26,16 +18,14 @@ function init_load() {
     "Iced Tea  -  $3.50"+"<br>"+
     "Milk  -  $2"+"<br>"+
     "Water  -  $1"+"<br>";
-    document.body.appendChild(menu_items);
+    menu.appendChild(menu_items);
+    document.body.appendChild(menu);
 
-    const contact_title = document.createElement('div');
-    contact_title.innerHTML="<br>"+"Contact Us"+"<br>";
-    contact_title.className="h1_text";
-    document.body.appendChild(contact_title);
-
+    const contact = document.createElement('div');
+    contact.className="page_content";
     const contact_text = document.createElement('div');
     contact_text.innerHTML="<br>"+"You can send your messenger pigeons or contact us digitally with the information given below:";
-    document.body.appendChild(contact_text);
+    contact.appendChild(contact_text);
 
     const contact_address = document.createElement('div');
     contact_address.innerHTML="<br>"+"10050 Cielo Drive"+"<br>"+
@@ -43,7 +33,8 @@ function init_load() {
     "VIC 3000"+"<br>"+
     "T: 0412 345 678"+"<br>"+
     "coffee@pazzos.com";
-    document.body.appendChild(contact_address);
+    contact.appendChild(contact_address);
+    document.body.appendChild(contact);
 }
 
 export default init_load
