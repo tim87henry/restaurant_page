bfd460a70a46938c11f341c6206a4657093f020e
Timothy <tim87henry@gmail.com>
2021-01-23 23:32:24 +1100

    Completed basic tabbed layout

--- src/index.js
+++ src/index.js
@@ -4,32 +4,36 @@ const banner = document.createElement('img');
 banner.src= "../banner.png";
 document.body.appendChild(banner);
 
+const new_line=document.createElement('br');
+document.body.appendChild(new_line);
+
 const about_title = document.createElement('button');
 about_title.innerHTML="About Us";
 about_title.className="h1_text";
-about_title.onclick=opentab;
+about_title.onclick=function() {opentab(event,"about");};
 document.body.appendChild(about_title);
 
 const menu_title = document.createElement('button');
 menu_title.innerHTML="Menu";
 menu_title.className="h1_text";
-menu_title.onclick=opentab;
+menu_title.onclick=function() {opentab(event,"menu");};
 document.body.appendChild(menu_title);
 
 const contact_title = document.createElement('button');
 contact_title.innerHTML="Contact Us";
 contact_title.className="h1_text";
-contact_title.onclick=opentab;
+contact_title.onclick=function() {opentab(event,"contact");};
 document.body.appendChild(contact_title);
 
 init_load();
 
-function opentab () {
-    //alert("Display individual tabs here");
+function opentab (e,section) {
     var pages;
 
     pages=document.getElementsByClassName("page_content");
     for (var i=0;i<pages.length;i++) {
         pages[i].style.display="none";
     }
+    var temp=document.querySelector("#"+section);
+    temp.style.display="block";
 }
--- src/init.js
+++ src/init.js
@@ -2,6 +2,7 @@ function init_load() {
     
     const about = document.createElement('div');
     about.className="page_content";
+    about.id="about";
     const about_text = document.createElement('div');
     about_text.innerHTML="<br>"+"Pazzo's Cafe is an authentic Italian cafe, established in the 5th century BC."+"<br>"+
     "We even sold take-away Lattes to Cleopatra and Julius Ceaser. In fact, we invented coffee.";
@@ -10,6 +11,7 @@ function init_load() {
 
     const menu = document.createElement('div');
     menu.className="page_content";
+    menu.id="menu";
     const menu_items = document.createElement('div');
     menu_items.innerHTML="<br>"+"Latte  -  $5.00"+"<br>"+
     "Capuccino  -  $5.50"+"<br>"+
@@ -23,6 +25,7 @@ function init_load() {
 
     const contact = document.createElement('div');
     contact.className="page_content";
+    contact.id="contact";
     const contact_text = document.createElement('div');
     contact_text.innerHTML="<br>"+"You can send your messenger pigeons or contact us digitally with the information given below:";
     contact.appendChild(contact_text);
