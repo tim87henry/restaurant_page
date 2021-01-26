3a8c7b28870caebfae81cb5039e74ad931de200e
Timothy <tim87henry@gmail.com>
2021-01-26 22:52:36 +1100

    Changed page layout

--- src/index.js
+++ src/index.js
@@ -2,31 +2,33 @@ import about_tab from './about';
 import menu_tab from './menu';
 import contact_tab from './contact';
 
-const banner = document.createElement('img');
-banner.id="banner";
-banner.src= "../banner.png";
-document.body.appendChild(banner);
-
-const new_line=document.createElement('br');
-document.body.appendChild(new_line);
+const navbar = document.createElement('div');
+navbar.id="navbar";
 
 const about_title = document.createElement('button');
 about_title.innerHTML="About Us";
 about_title.className="titles";
 about_title.onclick=function() {opentab(event,"about");};
-document.body.appendChild(about_title);
+navbar.appendChild(about_title);
 
 const menu_title = document.createElement('button');
 menu_title.innerHTML="Menu";
 menu_title.className="titles";
 menu_title.onclick=function() {opentab(event,"menu");};
-document.body.appendChild(menu_title);
+navbar.appendChild(menu_title);
 
 const contact_title = document.createElement('button');
 contact_title.innerHTML="Contact Us";
 contact_title.className="titles";
 contact_title.onclick=function() {opentab(event,"contact");};
-document.body.appendChild(contact_title);
+navbar.appendChild(contact_title);
+
+document.body.appendChild(navbar);
+
+const banner = document.createElement('img');
+banner.id="banner";
+banner.src= "../banner.png";
+document.body.appendChild(banner);
 
 about_tab();
 menu_tab();
