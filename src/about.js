cfbf73815bd71e34345afeed973535b5246192cd
Timothy <tim87henry@gmail.com>
2021-01-27 23:21:41 +1100

    Changed font and menu layout

--- src/about.js
+++ src/about.js
@@ -6,7 +6,14 @@ function about_tab() {
 
     const about_text = document.createElement('div');
     about_text.innerHTML="<br>"+"Pazzo's Cafe is an authentic Italian cafe, established in the 5th century BC."+"<br>"+
-    "We even sold take-away Lattes to Cleopatra and Julius Ceaser. In fact, we invented coffee.";
+    "We even sold take-away Lattes to Cleopatra and Julius Ceaser. In fact, we invented coffee."+"<br>"+"<br>"+"<br>"+
+    "Trading Hours"+"<br>"+
+    "<table id=\"trading\">"+
+    "<tr><td>Mon – Thu</td><td>07.00am – 5.00pm</td></tr>"+
+    "<tr><td>Fri – Sat</td><td>08.00am – 8.00pm</td></tr>"+
+    "<tr><td>Sun</td><td>08.00am – 1.00pm</td></tr>"+
+    "</table>"+"<br>"+
+    "Public Holidays: Closed"+"<br>"
     about.appendChild(about_text);
     document.body.append(about);
 }
--- src/menu.js
+++ src/menu.js
@@ -4,13 +4,22 @@ function menu_tab() {
     menu.className="page_content";
     menu.id="menu";
     const menu_items = document.createElement('div');
-    menu_items.innerHTML="<br>"+"Latte  -  $5.00"+"<br>"+
-    "Capuccino  -  $5.50"+"<br>"+
-    "Espresso  -  $4.50"+"<br>"+
-    "Flat White  -  $5.00"+"<br><br>"+
-    "Iced Tea  -  $3.50"+"<br>"+
-    "Milk  -  $2"+"<br>"+
-    "Water  -  $1"+"<br>";
+    menu_items.innerHTML="<br>"+
+    "<table id=\"menu_list\">"+
+    "<tr><td class=\"menu_item\">Latte</td><td class=\"cost\">$5.00</td></tr>"+
+    "<tr><td class=\"menu_item\">Capuccino</td><td class=\"cost\">$5.50</td></tr>"+
+    "<tr><td class=\"menu_item\">Espresso</td><td class=\"cost\">$4.50</td></tr>"+
+    "<tr><td class=\"menu_item\">Flat White</td><td class=\"cost\">$5.00</td></tr>"+
+    "<tr><td class=\"menu_item\">Hot Chocolate</td><td class=\"cost\">$6.00</td></tr>"+
+    "<tr><td></td><td></td></tr>"+
+    "<tr><td></td><td></td></tr>"+
+    "<tr><td class=\"menu_item\">Iced Latte</td><td class=\"cost\">$5.50</td></tr>"+
+    "<tr><td class=\"menu_item\">Iced Tea</td><td class=\"cost\">$4.50</td></tr>"+
+    "<tr><td></td><td></td></tr>"+
+    "<tr><td></td><td></td></tr>"+
+    "<tr><td class=\"menu_item\">Milk</td><td class=\"cost\">$2.00</td></tr>"+
+    "<tr><td class=\"menu_item\">Water</td><td class=\"cost\">$1.00</td></tr>"+
+    "</table>";
     menu.appendChild(menu_items);
     menu.style.display="none";
     document.body.appendChild(menu);
